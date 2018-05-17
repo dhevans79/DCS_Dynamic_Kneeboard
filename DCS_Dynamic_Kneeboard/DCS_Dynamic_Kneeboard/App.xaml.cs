cd30271506cb690c228af40fd02bcfb20e1660b5
David H Evans <dhevans79@gmail.com>
2018-05-17 13:07:44 +0200

    Added Sample Main Page with Initial navigation, and hidden the navigation bar

--- DCS_Dynamic_Kneeboard/DCS_Dynamic_Kneeboard/App.xaml.cs
+++ DCS_Dynamic_Kneeboard/DCS_Dynamic_Kneeboard/App.xaml.cs
@@ -11,7 +11,7 @@ namespace DCS_Dynamic_Kneeboard
 		{
 			InitializeComponent();
 
-			MainPage = new MainPage();
+			MainPage = new NavigationPage(new MainMenu());
 		}
 
 		protected override void OnStart ()
---
+++ DCS_Dynamic_Kneeboard/DCS_Dynamic_Kneeboard/MainMenu.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DCS_Dynamic_Kneeboard
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainMenu : ContentPage
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        void OnManageKneeboards(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ManageKneeboards());
+
+        }
+
+        /*async void OnManageKneeboards(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainMenu());
+        }*/
+
+    }
+}
